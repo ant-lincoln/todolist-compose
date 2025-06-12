fadcdd4ce4af269688c88dad210c36378e4c3d45
Lincoln Caetano <its-lincoln@hotmail.com>
2025-06-12 19:31:04 -0300

    Added events to delete and select category

--- app/src/main/java/com/seumelhorcaminho/todolist/ui/feature/list/ListEvent.kt
+++ app/src/main/java/com/seumelhorcaminho/todolist/ui/feature/list/ListEvent.kt
@@ -1,11 +1,15 @@
 package com.seumelhorcaminho.todolist.ui.feature.list
 
+import com.seumelhorcaminho.todolist.domain.Category
 import com.seumelhorcaminho.todolist.domain.Todo
 
 sealed interface ListEvent {
     data class OnDeleteClick(val todo: Todo) : ListEvent
     data class OnCompleteChange(val todo: Todo, val isCompleted: Boolean) : ListEvent
     data class OnItemClick(val id: Long?) : ListEvent
-
     data class OnAddCategory(val name: String, val emoji: String) : ListEvent
+
+    data class OnDeleteCategoryConfirm(val category: Category) : ListEvent
+    data class OnCategorySelected(val category: Category?) : ListEvent
+
 }
